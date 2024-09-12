ca39757178dd5ca4377a7b84036cad9ca869383b
Cristobal <cristobalsecofiastre@gmail.com>
2024-09-13 00:07:22 +0200

    Añadida estructura de carpetas compartidas, constantes, pipes, interfaces, enums, servicios / Añadido select para el modo oscuro/claro/sistema / Añadido tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,41 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if(isPlatformBrowser(this.platformId)) {
+      this.systemTheme()
+    }
+  }
+
+  changeTheme(typeTheme: any): void {
+    switch(typeTheme.target.value) {
+      case 'light':
+        this.lightTheme()
+        break
+      case 'dark':
+        this.darkTheme()
+        break
+      default:
+        this.systemTheme()
+        break
+    }
+  }
+
+  darkTheme(): void {
+    document.querySelector('body')?.classList.add('dark')
+  }
+
+  lightTheme(): void {
+    document.querySelector('body')?.classList.remove('dark')
+  }
+
+  systemTheme(): void {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.darkTheme();
+    } else {
+      this.lightTheme();
+    }
+  }
+
 }
