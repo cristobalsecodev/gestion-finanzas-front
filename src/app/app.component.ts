737c4f7f2df9aa71d70aba7085426d1e657161e5
Cristobal <cristobalsecofiastre@gmail.com>
2024-09-25 19:23:01 +0200

    Instalados componentes de spartan ng / Nuevo ejemplo de menú superior para la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeModes } from './shared/enums/ThemeModes.enum';
-import { FlowbiteService } from './services/flowbite/flowbite.service';
 import { SafeHtml } from '@angular/platform-browser';
 import { LOGO_SVG, THEME_SVGS } from './shared/constants/svg.constants';
 import { SecurizeSVGsService } from './services/securizeSVGs/securize-svgs.service';
@@ -10,35 +9,31 @@ import { SecurizeSVGsService } from './services/securizeSVGs/securize-svgs.servi
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet, 
+    CommonModule,
+    // Spartan UI
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  // Themes variables
-  themesModes = ThemeModes
-  themeSelected: string = this.themesModes.SYSTEM
   SVGs: { [key: string]: SafeHtml } = {}
   isSystemThemeDark: boolean = false;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object, 
-    private flowbiteService: FlowbiteService,
     private securizeSVGsService: SecurizeSVGsService
   ) {
     
     // Securizar cada SVG
     this.SVGs = this.securizeSVGsService.securizeSVGs({ ...THEME_SVGS, Logo: LOGO_SVG })
 
-    // Check if we are entering from a browser
+    // Comprueba si accedemos desde un navegador
     if(isPlatformBrowser(this.platformId)) {
       this.isSystemThemeDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     }
 
-  }
-  
-  ngOnInit(): void {
-    this.flowbiteService.loadFlowbite(flowbite => {});
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"
+    './src/**/*.{html,ts}',
+    './src/app/components/**/*.{html,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Color primario
+        primary: {
+          50:  '#eefffc',
+          100: '#c3fffa',
+          200: '#88fff6',
+          300: '#44fff1',
+          400: '#0cedde', // Color por defecto
+          500: '#00d6cb',
+          600: '#00ada6',
+          700: '#008986',
+          800: '#036c6b',
+          900: '#085958',
+          950: '#003537'
+        },
+        // Color primario
+        secondary: {
+          50:  '#f7f6fc',
+          100: '#f1eef9',
+          200: '#e5dff5',
+          300: '#d0c5ed',
+          400: '#b8a3e2', // Color por defecto
+          500: '#9f7ed4',
+          600: '#8c5ec5',
+          700: '#7d4fb2',
+          800: '#694196',
+          900: '#57377b',
+          950: '#372253'
+        },
+      }
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
   darkMode: 'class'
 }
 
