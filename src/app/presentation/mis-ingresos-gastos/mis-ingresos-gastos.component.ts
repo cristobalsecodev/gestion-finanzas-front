728b69824fb09973467288137cbc4c3e665faedd
Cristobal <cristobalsecofiastre@gmail.com>
2024-10-21 00:14:19 +0200

    Nuevo componente para tratar notifiaciones/errores / Nueva paleta de colores / Modificados estilos de componentes / Creado apartado de gráficos en ingresos/gastos

--- src/app/presentation/mis-ingresos-gastos/mis-ingresos-gastos.component.ts
+++ src/app/presentation/mis-ingresos-gastos/mis-ingresos-gastos.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { TablaGeneralComponent } from 'src/app/shared/components/tabla-general/tabla-general.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-mis-ingresos',
   standalone: true,
   imports: [
-    TablaGeneralComponent
+    TablaGeneralComponent,
+    MatCardModule
   ],
   templateUrl: './mis-ingresos-gastos.component.html',
   styleUrl: './mis-ingresos-gastos.component.scss'
